e5ee3d3fca82ef7747da405a2d21cb34d3a19929
Rodrigo-Sena <sena.rodrigo@outlook.com>
2020-08-06 18:27:47 +0100

    update EditEstablishment. not saving on DB yet.

--- src/components/establishment/EditEstablishment.js
+++ src/components/establishment/EditEstablishment.js
@@ -1,130 +1,251 @@
-import React, {useState} from 'react';
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
-import userApi from "../../apis/users"
-
+import userApi from "../../apis/users";
 
 const EditEstablishment = () => {
-    const {id} = useParams()
+  const { id } = useParams();
+  const history = useHistory();
 
-    const [profile, setProfile] = useState({
-        username: "",
-        password: "",
-        establishment: {
-          companyName: "",
-          category: "",
-          cuisine: [], 
-          phone: "",
-          email: "",
-          address: {
-            street: "",
-            number: 0,
-            complement: "",
-            neighborhood: "",
-            city: ""
-          },
-          paymentForms: [], 
-          offers: [],
-          delivery: true,
-          takeaway: true,
-          takeawayTime: {
-            startTime: "", 
-            endTime: ""
-          },
-          sales: []
-        }
-      });
+  const [profile, setProfile] = useState({
+    companyName: "",
+    category: "",
+    cuisine: [],
+    phone: "",
+    email: "",
+    address: {
+      street: "",
+      number: 0,
+      complement: "",
+      neighborhood: "",
+      city: "",
+    },
+    paymentForms: [],
+    offers: [],
+    delivery: true,
+    takeaway: true,
+    takeawayTime: {
+      startTime: "",
+      endTime: "",
+    },
+    sales: [],
+  });
+  
+  function handleChange(event) {
+    setProfile({
+      ...profile,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  }
 
-      function handleChange(event){
-        
-        setOffer({...profile,[event.currentTarget.name]: event.currentTarget.value})
+  useEffect(() => {
+    (async function fetchUser() {
+      try {
+        const result = await userApi.get(`/establishment/profile/${id}`);
 
+        setProfile({ ...result.data.establishment });
+      } catch (error) {
+        console.log(error);
       }
+    })();
+  }, []);
 
-      useEffect(() => {
-        (async function fetchUser() {
-          try {
-            const result = await userApi.get(`/establishment/profile/${id}`);
-    
-            setProfile({ ...result.data[0] });
-          } catch (error) {
-            console.log(error);
-          }
-        })();
-      }, []);
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-      async function handleSubmit(event) {
-        event.preventDefault();
-        
-        try {
-          const result = await userApi.put(`/establishment/profile/update/${id}`, profile);
-          console.log(result)
-          history.push(`/establishment/profile/${id}`);
-          history.go();
-        } catch (err) {
-          console.error(err);
-        }
-      }
+    try {
+      const result = await userApi.put(
+        `/establishment/profile/update/${id}`,
+        profile
+      );
+      
+      console.log(profile);
+      //   history.push(`/establishment/profile/${id}`);
+      //   history.go();
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-
-    return ( 
-        <div><h1>Editar Oferta</h1>
-    <hr className = "form-group"></hr>
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="nomePrato">Nome do Prato (title)</label>
-        <input
-          type="text"
-          className="form-control form-control-lg col-5"
-          id="offerTitleField"
-          name="title"
-          aria-describedby="emailHelp"
-          onChange={handleChange}
-          value={offer.title}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="valorPrato">Valor do prato (Value)</label>
-        <input
-          className="form-control form-control-lg col-5"
-          name="value"
-          id="offerValueField"
-          onChange={handleChange}
-          value={offer.value}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="fotoPrato">Foto do Prato (photo)</label>
-        <input
-          type="text"
-          className="form-control form-control-lg col-5"
-          id="offerPhotoField"
-          name="photo"
-          aria-describedby="emailHelp"
-          onChange={handleChange}
-          value={offer.photo}
-        />
-      </div>
-      <div className="form-group">
-      <label htmlFor="quantidadeDisp">Quantidade Disponível</label>
-        <input
-          className="form-control form-control-lg col-5"
-          name="availableQty"
-          id="offerValueField"
-          onChange={handleChange}
-          value={offer.availableQty}
-        />
+  return (
+    <div>
+      <h1>Editar Profile</h1>
+      <hr className='form-group'></hr>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='companyName'>Nome da Empresa</label>
+          <input
+            type='text'
+            className='form-control form-control-lg col-5'
+            id='companyNameField'
+            name='companyName'
+            aria-describedby='emailHelp'
+            onChange={handleChange}
+            value={profile.companyName}
+          />
         </div>
-        <div>
-          <input type="date" />
+        <div className='form-group'>
+          <label htmlFor='category'>Tipo de Estabelecimento</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='category'
+            id='categoryField'
+            onChange={handleChange}
+            value={profile.category}
+          />
         </div>
-      <button type="submit" className="btn btn-lg btn-primary">
-        Save Edit
-      </button>
-    </form>
+        <div className='form-group'>
+          <label htmlFor='cuisine'>Tipo de Cozinha</label>
+          <input
+            type='text'
+            className='form-control form-control-lg col-5'
+            id='cuisineField'
+            name='photo'
+            aria-describedby='emailHelp'
+            onChange={handleChange}
+            value={profile.cuisine}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Telefone</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='availableQty'
+            id='offerValueField'
+            onChange={handleChange}
+            value={profile.phone}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>E-mail</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='email'
+            id='emailField'
+            onChange={handleChange}
+            value={profile.email}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='phone'>Endereço</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='street'
+            id='streetField'
+            onChange={handleChange}
+            value={profile.address.street}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Número</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='number'
+            id='numberField'
+            onChange={handleChange}
+            value={profile.address.number}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Complemento</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='complement'
+            id='complementField'
+            onChange={handleChange}
+            value={profile.address.complement}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Bairro</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='neighborhood'
+            id='neighborhoodField'
+            onChange={handleChange}
+            value={profile.address.neighborhood}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Cidade</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='city'
+            id='offerValueField'
+            onChange={handleChange}
+            value={profile.address.city}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Formas de Pagamento</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='paymentForms'
+            id='paymentFormsField'
+            onChange={handleChange}
+            value={profile.paymentForms}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Entrega</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='delivery'
+            id='deliveryField'
+            onChange={handleChange}
+            value={profile.delivery}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Retira</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='takeaway'
+            id='takeawayField'
+            onChange={handleChange}
+            value={profile.takeaway}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Horário retirada inicio</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='startTime'
+            id='startTimeField'
+            onChange={handleChange}
+            value={profile.takeawayTime.startTime}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='phone'>Horário Retirada Final</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='endTime'
+            id='endTimeField'
+            onChange={handleChange}
+            value={profile.takeawayTime.endTime}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='phone'>Photo</label>
+          <input
+            className='form-control form-control-lg col-5'
+            name='availableQty'
+            id='offerValueField'
+            onChange={handleChange}
+            value={profile.phone}
+          />
+        </div>
+
+        <button type='submit' className='btn btn-lg btn-primary'>
+          Save Edit
+        </button>
+      </form>
     </div>
-     );
-}
- 
+  );
+};
 
 export default EditEstablishment;
