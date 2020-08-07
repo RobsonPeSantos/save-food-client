753a4a5d79238b6385be9a93e4a87b8b63888939
Robson <robsonoliveira@gmail.com>
2020-08-07 03:04:50 -0300

    about us and how it works styles added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 const Home = (props) => {
   return (
     <div className="home-background">
-    
       <Form
         allOffers={props.allOffers}
         offers={props.offers}
@@ -13,18 +12,24 @@ const Home = (props) => {
       ></Form>
 
       <div className="homeLogo">
-        <div >
-        <img src="https://res.cloudinary.com/savefood/image/upload/v1596761593/save-food/LogoGrafismo_vdfqst.png" alt="" style={{width: 150, margin: 10}} />
+        <div>
+          <img
+            src="https://res.cloudinary.com/savefood/image/upload/v1596761593/save-food/LogoGrafismo_vdfqst.png"
+            alt=""
+            style={{ width: 150, margin: 10 }}
+          />
         </div>
         <div>
-        <h1 style={{color: "white", fontSize: 50}}>Porque comida não se joga fora</h1>
+          <h1 style={{ color: "white", fontSize: 50 }}>
+            Porque comida não se joga fora
+          </h1>
         </div>
         <div>
-        <h4 style={{color: "white"}}>Seja um estabelecimento participante</h4>
+          <h4 style={{ color: "white" }}>
+            Seja um estabelecimento participante
+          </h4>
         </div>
-        
       </div>
-      
     </div>
   );
 };
