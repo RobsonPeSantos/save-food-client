b4133a239f44357f477da68fde2a2e2de18bd4ea
Robson <robsonoliveira@gmail.com>
2020-08-14 14:41:53 -0300

    all offers styles changed to add responsivity

--- src/components/Offer/AllOffers.js
+++ src/components/Offer/AllOffers.js
@@ -11,31 +11,33 @@ const AllOffers = (props) => {
         offers={props.offers}
         setOffers={props.setOffers}
       ></Form>
-      <div className="offersBody">
-        {props.offers.map((offer) => (
-          <div
-            className="card"
-            style={{ width: "60rem", padding: "5px", color: "#276359" }}
-          >
-            <img
-              className="card-img-top"
-              src={offer.photo}
-              alt="Cardimagecap"
-            />
-            <div className="card-body">
-              <h5 className="card-title">{`R$ ${offer.value}`}</h5>
-              <p className="card-text">{offer.title}</p>
-              <Link
-                type="button"
-                className="btn btn-primary"
-                style={{ backgroundColor: "#276359" }}
-                to={`offer/${offer._id}`}
-              >
-                RESERVAR
-              </Link>
+      <div className="external-body">
+        <div className="offersBody">
+          {props.offers.map((offer) => (
+            <div
+              className="card"
+              style={{ width: "60rem", padding: "5px", color: "#276359" }}
+            >
+              <img
+                className="card-img-top"
+                src={offer.photo}
+                alt="Cardimagecap"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{`R$ ${offer.value}`}</h5>
+                <p className="card-text">{offer.title}</p>
+                <Link
+                  type="button"
+                  className="btn btn-primary"
+                  style={{ backgroundColor: "#276359", borderColor: "#276359" }}
+                  to={`offer/${offer._id}`}
+                >
+                  RESERVAR
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
